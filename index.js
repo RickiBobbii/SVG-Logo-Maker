ac0c9db587bb0024bcf2166cf83554eef967aa8a
RickiBobbii <rickibobbiii@gmail.com>
2023-10-29 12:19:32 -0400

    added intial shapes code to prep for testing

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const Circle = require('./lib/shapes');
+const Triangle = require('./lib/shapes');
+const Square = require('./lib/shapes');
 
 inquirer
     .prompt([
@@ -32,16 +35,18 @@ inquirer
         const logoSvg =
         `<!DOCTYPE html>
         <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
-        </head>
         <body>
             <p>${answers.title}</p>
             <p>${answers.textColor}</p>
             <p>${answers.shape}</p>
             <p>${answers.shapeColor}</p>
+        <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+        <${answers.shape} cx="150" cy="100" r="80" fill="${answers.shapeColor}" />
+      
+        <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.title}</text>
+      
+      </svg>
         </body>
         </html>`;
     fs.writeFile('./logo.svg', logoSvg, (err) => {
